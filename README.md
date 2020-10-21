71c4afda09d1f40a777a9a1a66b31abb29a5cc37
KaKeimei <hqm237594742@gmail.com>
2020-10-22 04:02:10 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# MetaSV stream connect performance
+
+This program is to test the performance for connections on the new MetaSV stream api.
+
+## Usage
+
+Change the request header , add the api key to finish the test, if the key is not present, you will get 429
+
+```
+    // todo replace the api-key
+	request.Header.Add("x-api-key", "ask-me-for-the-key")
+	request.Header.Set("Content-Type", "application/json")
+```
+
+Change the number of go routines to see how is the performance
+
+## Test Result
+
+Maximum 2000 active connections running on local PC. Getting out of memory locally if exceed this number.
+
+So the server can handle at least 2000 stream connections
---
+++ main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"net/http"
+	"time"
+)
+
+func main() {
+	// set go routine(thread) count here
+	for i := 0; i < 2000; i++ {
+		go requestAddress()
+		time.Sleep(time.Millisecond * 20)
+	}
+
+	select {}
+
+}
+
+// create a go routine to keep connection to the address stream
+// if any of the connection is lost, the program panics
+func requestAddress() {
+	request, _ := http.NewRequest("GET", "https://api.metasv.com/v1/stream/address/1GjJXSYp9DhkvNV8NXfBAecpa5PhtEvpw8", nil)
+	// todo replace the api-key
+	request.Header.Add("x-api-key", "ask-me-for-the-key")
+	request.Header.Set("Content-Type", "application/json")
+	clt := http.Client{}
+	resp, _ := clt.Do(request)
+
+	reader := bufio.NewReader(resp.Body)
+	for {
+		line, _, err := reader.ReadLine()
+		if err != nil && err != io.EOF {
+			break
+		}
+		fmt.Println(string(line))
+		if err != nil {
+			break
+		}
+	}
+	panic("connection finished")
+}
